83dc269940ecbad6bf78ed947bf48c0fe591b657
ETblaky <tiago.bannwart@gmail.com>
2016-11-15 15:51:31 -0200

    When there are 15, 10, 5, 4, 3, 2, and 1 second remaining play XP sound.

--- src/main/java/me/etblaky/hg/Lobby/LobbyTimer.java
+++ src/main/java/me/etblaky/hg/Lobby/LobbyTimer.java
@@ -37,47 +37,48 @@ public class LobbyTimer {
                     switch (time){
                         case (120 - 15):
                             TitleApi.sendTitle(p, ChatColor.GREEN + "15 segundos", 1, 1, 0);
-                            p.playSound(p.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 1);
+                            p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
                             break;
                         case (120 - 10):
                             TitleApi.sendTitle(p, ChatColor.GREEN + "10 segundos", 1, 1, 0);
-                            p.playSound(p.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 1);
+                            p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
                             break;
                         case (120 - 5):
                             TitleApi.sendTitle(p, ChatColor.GREEN + "5 segundos", 1, 1, 0);
-                            p.playSound(p.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 1);
+                            p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
                             break;
                         case (120 - 4):
                             TitleApi.sendTitle(p, ChatColor.DARK_GREEN + "4 segundos", 1, 1, 0);
-                            p.playSound(p.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 1);
+                            p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
                             break;
                         case (120 - 3):
                             TitleApi.sendTitle(p, ChatColor.YELLOW + "3 segundos", 1, 1, 0);
-                            p.playSound(p.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 1);
+                            p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
                             break;
                         case (120 - 2):
                             TitleApi.sendTitle(p, ChatColor.RED + "2 segundos", 1, 1, 0);
-                            p.playSound(p.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 1);
+                            p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
                             break;
                         case (120 - 1):
                             TitleApi.sendTitle(p, ChatColor.DARK_RED + "1 segundo", 1, 1, 0);
-                            p.playSound(p.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 1);
+                            p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
                             break;
                     }
 
                     lobby.getBoard().setUpPlayer(p);
 
-                    if(time == 120){
-                        if(lobby.getPlayers().size() > 0){
-                            lobby.stop();
-                            return;
-                        }
+                }
 
-                        TitleApi.sendTitle(p, "Não tem jogadores suficientes para iniciar a partida!", 1, 2, 1 );
-
-                        time = 0;
-
+                if(time == 120){
+                    if(lobby.getPlayers().size() > 0){
+                        lobby.stop();
+                        return;
                     }
+                    for(Player p : lobby.getPlayers()){
+                        TitleApi.sendTitle(p, "Não tem jogadores suficientes para iniciar a partida!", 1, 2, 1 );
+                    }
+
+                    time = 0;
 
                 }
 
